b5d4e3a2946ebd2e99e0203b69db0ddcd84f8162
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-10-30 03:41:08 +0200

    feat: implement header

--- christmas-shop/src/js/main.js
+++ christmas-shop/src/js/main.js
@@ -1,10 +1,1 @@
 import '../scss/style.scss'
-
-const h2 = document.querySelector('h2')
-
-h2.addEventListener('mouseover', () => {
-  h2.innerText = 'mouseover'
-})
-h2.addEventListener('mouseout', () => {
-  h2.innerText = 'mouseout'
-})
