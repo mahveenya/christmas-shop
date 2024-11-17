20151891a7f58b9dc168c92f0e840bb4c974bf80
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-11-17 22:57:05 +0200

    fix: prevent same page reload

--- christmas-shop/src/js/router.js
+++ christmas-shop/src/js/router.js
@@ -2,6 +2,11 @@ import { populateGiftsSection } from '../js/populateGiftsSection'
 import { index } from '../templates/index'
 import { gifts } from '../templates/gifts'
 
+const routes = {
+  '/mahveenya-JSFE2024Q4/christmas-shop/': index,
+  '/mahveenya-JSFE2024Q4/christmas-shop/gifts': gifts,
+}
+
 document.addEventListener('click', (e) => {
   const element = e.target.closest('a, button, [data-url]')
   if (!element) return
@@ -10,17 +15,22 @@ document.addEventListener('click', (e) => {
 
   const url = element.dataset?.url || element?.href
 
-  e.preventDefault()
-  route(url)
+  if (url) {
+    e.preventDefault()
+    route(url)
+  }
 })
 
-const routes = {
-  '/mahveenya-JSFE2024Q4/christmas-shop/': index,
-  '/mahveenya-JSFE2024Q4/christmas-shop/gifts': gifts,
-}
-
 const route = (url) => {
-  window.history.pushState({}, '', url)
+  let urlPath = url
+  if (url.includes('http')) urlPath = new URL(url).pathname
+
+  const currentState = window.history.state || {}
+  const isSamePath = currentState.path === urlPath
+
+  if (isSamePath) return
+
+  window.history.pushState({ path: urlPath }, '', urlPath)
 
   locationHandler()
   window.scrollTo({ top: 0 })
