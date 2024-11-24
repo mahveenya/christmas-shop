2e8c67ff1cbb32b8e2f0e82e03b0d7006cf12593
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-11-24 14:00:59 +0200

    reafactor: use json instead of js for giftcards

--- christmas-shop/src/js/utilities.js
+++ christmas-shop/src/js/utilities.js
@@ -1,12 +1,13 @@
-import giftsData from '../assets/data/gifts'
+import gifts from '../assets/data/gifts'
 
 export function getRandomGifts(numOfGifts) {
-  if (numOfGifts > giftsData.length) {
+  const { data } = gifts
+  if (numOfGifts > data.length) {
     throw new Error(
       `Number of required cards to be returned cannot be more than ${giftsData.length}`
     )
   }
 
-  const shuffledArray = giftsData.slice().sort(() => Math.random() - 0.5)
+  const shuffledArray = data.slice().sort(() => Math.random() - 0.5)
   return shuffledArray.slice(0, numOfGifts)
 }
